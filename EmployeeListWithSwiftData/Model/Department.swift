ffa8fad9dc7de0150e4631d2d4dfac56116a168e
Gaurav Tak <gaurav.tak@roro.io>
2023-07-27 23:32:16 +0530

    bundle id updated and minor changes

--- EmployeeListWithSwiftData/Model/Department.swift
+++ EmployeeListWithSwiftData/Model/Department.swift
@@ -2,7 +2,7 @@
 //  Department.swift
 //  EmployeeListWithSwiftData
 //
-//  Created by Roro Solutions LLP on 13/06/23.
+//  Created by Gaurav Tak on 13/06/23.
 //
 
 import Foundation
--- EmployeeListWithSwiftData/Views/EmployeeListView.swift
+++ EmployeeListWithSwiftData/Views/EmployeeListView.swift
@@ -2,7 +2,7 @@
 //  EmployeeListView.swift
 //  EmployeeListWithSwiftData
 //
-//  Created by Roro Solutions LLP on 13/06/23.
+//  Created by Gaurav Tak on 13/06/23.
 //
 
 
